0118e868b121cab0929b45f0d7778d40cffe7957
h06604 <a0989659402@gmail.com>
2019-09-27 14:17:17 +0800

    two pair encoding complete

--- test.py
+++ test.py
@@ -12,23 +12,23 @@ from cmd import Cmd
 from os import isatty
 
 
-h1,h2,s1,s2,h11,h22 = net.get('h1','h2','s1','s2','h11','h22')
-h1.intf( 'h1-eth0' ).config(bw = 20)
-h2.intf( 'h2-eth0' ).config(bw = 20)
-h11.intf( 'h11-eth0' ).config(bw = 20)
-h22.intf( 'h22-eth0' ).config(bw = 20)
-s1.intf('s1-eth1').config(bw = 20)
-s1.intf('s1-eth2').config(bw = 20)
-s1.intf('s1-eth3').config(bw = 20)
-s2.intf('s2-eth1').config(bw = 20)
-s2.intf('s2-eth2').config(bw = 20)
-s2.intf('s2-eth3').config(bw = 20)
+#h1,h2,s1,s2,h11,h22 = net.get('h1','h2','s1','s2','h11','h22')
+#h1.intf( 'h1-eth0' ).config(bw = 20)
+#h2.intf( 'h2-eth0' ).config(bw = 20)
+#h11.intf( 'h11-eth0' ).config(bw = 20)
+#h22.intf( 'h22-eth0' ).config(bw = 20)
+#s1.intf('s1-eth1').config(bw = 20)
+#s1.intf('s1-eth2').config(bw = 20)
+#s1.intf('s1-eth3').config(bw = 20)
+#s2.intf('s2-eth1').config(bw = 20)
+#s2.intf('s2-eth2').config(bw = 20)
+#s2.intf('s2-eth3').config(bw = 20)
 
 
-list1 = [h1,h2]
-list2 = [h11,h22]
+#list1 = [h1,h2]
+#list2 = [h11,h22]
 
-net.iperf(hosts=list1,l4Type = 'UDP',udpBw='30M',seconds=5,port=5002)
+#net.iperf(hosts=list1,l4Type = 'UDP',udpBw='30M',seconds=5,port=5002)
 
 
 #net.iperf(hosts=list2,l4Type = 'UDP',udpBw='30M',seconds=5,port=5001)
